2b7225d0fe0c55cbe663b3e65b386393969b444b
Matthew Howlett <howl0893@kettering.edu>
2022-02-02 22:26:33 -0700

    more updates to the UI

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -2,20 +2,44 @@
   <div class="main" id="app">
     <navbar />
     <img
-      v-if="userTheme === 'dark-theme'"
-      src="../assets/day-moon-dark.jpg"
+      v-if="userTheme === 'light-theme'"
+      src="../assets/day-moon.jpg"
       width="100%"
     />
-    <img v-else src="../assets/day-moon.jpg" width="100%" />
+    <img v-else src="../assets/day-moon-dark.jpg" width="100%" />
+    <h1>Welcome to the home page</h1>
+    <p class="welcome">
+      ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+      nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate
+    </p>
     <div class="row">
       <div class="column">
-        <div class="card">Projects</div>
+        <div class="card">Past
+          <p class="card-info">
+            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          </p>
+        </div>
       </div>
       <div class="column">
-        <div class="card">About</div>
+        <div class="card">
+          Present
+          <p class="card-info">
+            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          </p>
+        </div>
       </div>
       <div class="column">
-        <div class="card">Contact</div>
+        <div class="card">
+          Future
+          <p class="card-info">
+            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -33,16 +57,16 @@ export default {
   data() {
     return {
       userTheme: null,
-      card: {
-        img: "image-equilibrium.jpg",
-        name: "Equilibrium",
-        description: "Our Equilibrium collection promotes balance and calm.",
-        id: 3429,
-        price: 0.041,
-        time: 3,
-        autor: "Jules Wyvern",
-        avatar: "image-avatar.png",
-      },
+      // card: {
+      //   img: "image-equilibrium.jpg",
+      //   name: "Equilibrium",
+      //   description: "Our Equilibrium collection promotes balance and calm.",
+      //   id: 3429,
+      //   price: 0.041,
+      //   time: 3,
+      //   autor: "Jules Wyvern",
+      //   avatar: "image-avatar.png",
+      // },
     };
   },
   mounted() {
@@ -56,37 +80,50 @@ export default {
 <style scoped lang="css">
 @import "../styles/theme.css";
 
+h1 {
+  /* font-weight: bold; */
+  color: var(--text-primary-color);
+  text-align: center;
+  padding: 20px;
+}
+
+.welcome {
+  width: 40%;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 20px;
+}
+
+.card-info {
+  color: var(--text-primary-color);
+  text-align: left;
+  border-radius: 4px;
+  border: 1px solid var(--accent-color);
+  height: 100px;
+  padding: 10px
+}
+
 * {
   box-sizing: border-box;
 }
 
-body {
-  font-family: Arial, Helvetica, sans-serif;
-}
-
 /* Float four columns side by side */
 .column {
-  float: left;
   width: 25%;
-  padding: 20px 20px;
-}
-
-/* Remove extra left and right margins, due to padding in columns */
-.row {margin: 0 -5px;}
-
-/* Clear floats after the columns */
-.row:after {
-  content: "";
-  display: table;
-  clear: both;
+  padding: 20px;
+  margin-left: auto;
+  margin-right: auto;
 }
 
 /* Style the counter cards */
 .card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); /* this adds the "card" effect */
+  box-shadow: 16px 16px 16px 0 rgba(28, 153, 122, 0.2); /* this adds the "card" effect */
+  border-radius: 4px;
+  border: 1px solid var(--accent-color);
   padding: 16px;
   text-align: center;
   background-color: var(--background-color-primary);
+  height: 300px;
 }
 
 /* Responsive columns - one column layout (vertical) on small screens */
@@ -96,5 +133,11 @@ body {
     display: block;
     margin-bottom: 20px;
   }
+  .welcome {
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 20px;
+}
 }
 </style>
--- frontend/src/components/ThemeButton.vue
+++ frontend/src/components/ThemeButton.vue
@@ -20,7 +20,7 @@ export default {
 
   data() {
     return {
-      userTheme: "light-theme",
+      userTheme: "dark-theme",
     };
   },
   
