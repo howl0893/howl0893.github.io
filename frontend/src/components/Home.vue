9442ed33dd6c5dea724701fc5515d3f7e5a3d98d
Matthew Howlett <matt.r.howlett@gmail.com>
2022-02-02 20:52:27 -0700

    begin to add cards

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -7,54 +7,42 @@
       width="100%"
     />
     <img v-else src="../assets/day-moon.jpg" width="100%" />
-    <p>
-      ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisi vel
-      consectetur interdum, nisl nunc interdum nisi, euismod aliquam nisl nunc
-      euismod nisl. Donec euismod, nisi vel consectetur interdum, nisl nunc
-      interdum nisi, euismod aliquam nisl nunc euismod nisl. Donec euismod, nisi
-      vel consectetur interdum, nisl nunc interdum nisi, euismod aliquam nisl
-      nunc euismod nisl. Donec euismod, nisi vel consectetur interdum, nisl nunc
-      interdum nisi, euismod aliquam nisl nunc euismod nisl. Donec euismod, nisi
-      vel consectetur interdum, nisl nunc interdum nisi, euismod aliquam nisl
-      nunc euismod nisl. Donec euismod, nisi vel consectetur interdum, nisl nunc
-      interdum nisi, euismod aliquam nisl nunc euismod nisl. Donec euismod, nisi
-      vel consectetur interdum, nisl nunc interdum nisi, euismod aliquam nisl
-      nunc euismod nisl. Donec euismod, nisi vel consectetur
-    </p>
-    <p>
-      interdum, nisl nunc interdum nisi, euismod aliquam nisl nunc euismod nisl.
-      Donec euismod, nisi vel consectetur interdum, nisl nunc interdum nisi,
-      euismod aliquam nisl nunc euismod nisl. Donec euismod, nisi vel
-      consectetur interdum, nisl nunc interdum nisi, euismod aliquam nisl nunc
-      euismod nisl. Donec
-    </p>
-    <p>
-      ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisi vel
-      consectetur interdum, nisl nunc interdum nisi, euismod aliquam nisl nunc
-      euismod nisl. Donec euismod, nisi vel consectetur interdum, nisl nunc
-      interdum nisi, euismod aliquam nisl nunc euismod nisl. Donec euismod, nisi
-      vel consectetur interdum, nisl nunc interdum nisi, euismod aliquam nisl
-      nunc euismod nisl. Donec euismod, nisi vel consectetur interdum, nisl nunc
-      interdum nisi, euismod aliquam nisl nunc euismod nisl. Donec euismod, nisi
-      vel consectetur interdum, nisl nunc interdum nisi, euismod aliquam nisl
-      nunc euismod nisl. Donec euismod, nisi vel consectetur interdum, nisl nunc
-      interdum nisi, euismod aliquam nisl nunc euismod nisl. Donec euismod, nisi
-      vel consectetur interdum, nisl nunc interdum nisi, euismod aliquam nisl
-      nunc euismod nisl. Donec euismod, nisi vel consectetur
-    </p>
+    <div class="row">
+      <div class="column">
+        <div class="card">Projects</div>
+      </div>
+      <div class="column">
+        <div class="card">About</div>
+      </div>
+      <div class="column">
+        <div class="card">Contact</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import navbar from "./NavBar.vue";
+// import NFTCard from "./NFTCard.vue";
 
 export default {
   components: {
     navbar,
+    // NFTCard,
   },
   data() {
     return {
       userTheme: null,
+      card: {
+        img: "image-equilibrium.jpg",
+        name: "Equilibrium",
+        description: "Our Equilibrium collection promotes balance and calm.",
+        id: 3429,
+        price: 0.041,
+        time: 3,
+        autor: "Jules Wyvern",
+        avatar: "image-avatar.png",
+      },
     };
   },
   mounted() {
@@ -65,11 +53,48 @@ export default {
 };
 </script>
 
-<style>
-p {
-  max-width: 758px;
-  /* center paragraph on page */
-  margin: 0 auto;
-  padding: 15px;
+<style scoped lang="css">
+@import "../styles/theme.css";
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+/* Float four columns side by side */
+.column {
+  float: left;
+  width: 25%;
+  padding: 20px 20px;
+}
+
+/* Remove extra left and right margins, due to padding in columns */
+.row {margin: 0 -5px;}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Style the counter cards */
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); /* this adds the "card" effect */
+  padding: 16px;
+  text-align: center;
+  background-color: var(--background-color-primary);
+}
+
+/* Responsive columns - one column layout (vertical) on small screens */
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+    display: block;
+    margin-bottom: 20px;
+  }
 }
 </style>
---
+++ frontend/src/components/NFTCard.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="card">
+    <div class="card__product-img">
+      <img 
+        class="card__img"
+        alt="product-image" 
+        :src="require('@/static/' + img)"
+      >
+    </div>
+    <div class="card__content">
+      <p class="card__name">{{ name }} <span v-if="id">#{{ id }}</span></p>
+      <p class="card__description" v-if="description">{{ description }}</p>
+      <div class="card__content-bottom">
+        <div class="card__price">
+          <div class="icon">
+            <img src="@/assets/nft-card/icon-ethereum.svg" alt="">
+          </div>
+          <p>{{ price }} ETH</p>
+        </div>
+        <div v-if="time" class="card__time">
+          <div class="icon">
+            <img src="@/assets/nft-card/icon-clock.svg" alt="">
+          </div>
+          <p>{{ time }} days left</p>
+        </div>
+      </div>
+    </div>
+    <div class="card__footer">
+      <div class="card__avatar">
+        <img :src="require('@/static/' + avatar)" alt="avatar-image">
+      </div>
+      <p class="card__autor">Creation of <span class="card__autor--alt-color">{{ autor }}</span></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    img: {
+      type: String,
+      default: '',
+      required: true
+    },
+    name: {
+      type: String,
+      default: '',
+      required: true
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    id: {
+      type: [String, Number],
+      default: null
+    },
+    price: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+    time: {
+      type: [String, Number],
+      default: ''
+    },
+    autor: {
+      type: String,
+      default: ''
+    },
+    avatar: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style scoped>
+.icon {
+  display: flex;
+}
+.card {
+  background-color: #15263F;
+  color: #8BACD9;
+  border-radius: 16px;
+  padding: 24px;
+  width: 327px;
+  font-size: 1.5rem;
+  box-shadow: 0 25px 50px 0 rgba(0,0,0,0.1);
+  @media screen and (min-width: 768px) {
+    font-size: 1.6rem;
+    width: 350px;
+    padding-bottom: 32px;
+  }
+}
+  .card__product-img {
+    cursor: pointer;
+    position: relative;
+    border-radius: 8px;
+    overflow: hidden;
+    @mixin hoverOpacity {
+      content: "";
+      position: absolute;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      opacity: 0;
+      transition: opacity 0.25s ease-out;
+    }
+  }
+  .card__content {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    padding: 24px 0 16px 0;
+    @media screen and (min-width: 768px) {
+      gap: 16px;
+      padding: 24px 0;
+    }
+  }
+  .card__footer {
+    display: flex;
+    align-items: center;
+    border-top: 1px solid #2E405A;
+    gap: 16px;
+    padding-top: 16px;
+  }
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: 0.25s ease-out;
+}
+.fade-leave-to,
+.fade-enter-from {
+  opacity: 0;
+}
+</style>
