e922927d226ec9f7c40bd96bd1c9dfe7625847a4
Matthew Howlett <mhowlett@michiganaerospace.com>
2023-02-19 22:18:56 -0700

    styling updates

---
+++ frontend/src/components/Reconn.vue
@@ -0,0 +1,34 @@
+<template>
+    <body>
+      <div
+        id="doc"
+        class="container-fluid markdown-body comment-enabled"
+        data-hard-breaks="true"
+      >
+        <br />
+        <h1 style="margin-bottom: 7px">Reconn.AI</h1>
+        <small style="float: left; color: #888"> 2023 Feb 19 </small>
+        <!-- <small style="float: right; color: #888"><a href="/">Back</a></small> -->
+        <br />
+        <br />
+        <br />
+
+        <p>
+          I am one of three software developers contributing to this Machine learning powered website. A tool that helps automate the annotation process of camera trap imagery.
+  
+  
+        </p>
+        <p><img src="../assets/reconn/collection.png" width="90%"></p>
+      </div>
+    </body>
+  </template>
+  
+  <script>
+  </script>
+  
+  <style lang="css" scoped>
+  @import "styles.css";
+
+
+  </style>
+  
