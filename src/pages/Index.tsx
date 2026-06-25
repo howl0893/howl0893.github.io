05c0f4cd8cfd689b197b95804e532f9c8e4f1fb2
Matthew Howlett <matt@wildtrack.org>
2026-06-24 23:14:52 -0700

    scrollable landing page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,8 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
 const Index = () => {
@@ -8,6 +11,9 @@ const Index = () => {
       <Navbar />
       <main className="flex-1">
         <Hero />
+        <About />
+        <Projects />
+        <Contact />
       </main>
       <Footer />
     </div>
